1936fa914f08b28cf866dee482723912e1e2394d
EC2 Default User <ec2-user@ip-172-31-14-188.ap-northeast-1.compute.internal>
2022-11-14 10:32:07 +0000

    [Finish]2-9 演習問題

--- new.rb
+++ new.rb
@@ -1,20 +1,35 @@
 class Car
+
+   def move(direction, distance)
+    self.turn(direction)
+    self.run(distance)
+  end
+
   def turn(direction)
     puts "#{direction}曲がります"
   end
-  
+
   def run(distance)
     puts "車で#{distance}キロ走ります。"
   end
 end
 
 car = Car.new
-car.turn("右")
+car.move("右",5)
 
 car=Car.new
 car.run(5)
 
-  
-    
-  
+class Car
+  def self.run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+end
 
+class Car
+  def self.turn(direction)
+    puts "#{direction}に曲がります。"
+  end
+end
+
+Car.turn("右")
